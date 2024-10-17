701f1ce6cff76d14a0837ae209a3065ea7b0337d
Ashyam Zubair <ashyam.zubair@sarasanalytics.com>
2024-10-17 10:55:29 +0530

    refactor

--- utils/bigquery.py
+++ utils/bigquery.py
@@ -1,6 +1,5 @@
 import os
 from concurrent.futures import ThreadPoolExecutor, as_completed
-from datetime import datetime, timezone
 from typing import Any, Dict, List, Tuple
 
 from dotenv import load_dotenv
@@ -74,7 +73,7 @@ def process_dataset(
             audit_dataset_name=audit_dataset_name,
             latency_params_table=latency_params_table,
             dataset_id=dataset,
-            tables=", ".join([f"'{table}'" for table in config["tables"]]) if config["tables"] else "NULL"
+            tables=", ".join([f"'{table}'" for table in config["tables"]]) if config["tables"] else "NULL",
         )
         query_job = client.query(query)
         results = [dict(row) for row in query_job.result()]
@@ -83,8 +82,11 @@ def process_dataset(
     # Ensure 'hours_since_update' is present and valid in all results
     for result in all_results:
         if "hours_since_update" not in result or result["hours_since_update"] is None:
-            cprint(f"Warning: hours_since_update missing for {result.get('table_id', 'unknown table')}", severity="WARNING")
-            result["hours_since_update"] = float('inf')  # or some default value
+            cprint(
+                f"Warning: hours_since_update missing for {result.get('table_id', 'unknown table')}",
+                severity="WARNING",
+            )
+            result["hours_since_update"] = float("inf")  # or some default value
         else:
             result["hours_since_update"] = float(result["hours_since_update"])
 
